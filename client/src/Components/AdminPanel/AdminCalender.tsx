0a219dfd8c8e6258468bf9895c1616822950ac13
asif777 <sk.rahmanasif@gmail.com>
2023-06-24 19:48:44 +0600

    little changes

--- client/src/Components/AdminPanel/AdminCalender.tsx
+++ client/src/Components/AdminPanel/AdminCalender.tsx
@@ -5,10 +5,9 @@ import './AdminCalender.css';
 import { Value } from 'react-calendar/dist/cjs/shared/types';
 import AdminTimeSlots from './AdminTimeSlots/AdminTimeSlots';
 
-
 const AdminCalendar: React.FC = () => {
   const [selectedDates, setSelectedDates] = useState<Value>([]);
-  
+
   const [goClick, setGoClick] = useState<boolean>(false);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [calendarStyle, setCalendarStyle] = useState({
@@ -16,7 +15,7 @@ const AdminCalendar: React.FC = () => {
     transition: '',
     transform: '',
   });
-  
+
   const handleDateChange = (value: Value) => {
     if (value.length === 1) {
       setSelectedDates([value[0], value[0]]);
@@ -28,36 +27,37 @@ const AdminCalendar: React.FC = () => {
       transition: 'margin-left 3s ease',
       transform: 'scale(0.8)',
     });
-    
   };
 
   return (
-    <div className=''>
+    <div>
+      <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+        Book your calender
+      </h1>
       <div className='flex gap-4'>
-        <div  className=''>
+        <div className=''>
           <Calendar
-          
             onChange={handleDateChange}
             value={selectedDates}
             selectRange={true}
             isMultiSelection={true}
             showNeighboringMonth={false} // Hide neighboring months in the calendar
           />
-       <br />
-        {selectedDates.length > 0 ? (
-          <p className='text-center date-range'>
-            <span className='bold'>Start:</span>{' '}
-            {selectedDates[0].toDateString()}
-            &nbsp;|&nbsp;
-            <span className='bold'>End:</span>{' '}
-            {selectedDates[selectedDates.length - 1].toDateString()}
-          </p>
-        ) : (
-          <p className='text-center'>
-            <span className='bold'>Please select a date range.</span>
-          </p>
-        )}
-         </div>
+          <br />
+          {selectedDates.length > 0 ? (
+            <p className='text-center date-range'>
+              <span className='bold'>Start:</span>{' '}
+              {selectedDates[0].toDateString()}
+              &nbsp;|&nbsp;
+              <span className='bold'>End:</span>{' '}
+              {selectedDates[selectedDates.length - 1].toDateString()}
+            </p>
+          ) : (
+            <p className='text-center'>
+              <span className='bold'>Please select a date range.</span>
+            </p>
+          )}
+        </div>
         <AdminTimeSlots />
       </div>
     </div>
