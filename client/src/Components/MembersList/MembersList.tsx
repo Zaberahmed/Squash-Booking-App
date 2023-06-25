431f5724d88dab0da4c1dd8cead2a7d59728eeac
asif777 <sk.rahmanasif@gmail.com>
2023-06-25 19:37:13 +0600

    register navigation done

--- client/src/Components/MembersList/MembersList.tsx
+++ client/src/Components/MembersList/MembersList.tsx
@@ -42,7 +42,7 @@ const MembersList = () => {
     <div className='container'>
       <div className='top-right'>
         <button
-          className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'
+          className='bg-transparent hover:bg-orange-500 text-black-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded'
           onClick={routeChange}
         >
           +Add Member
--- client/src/Components/Register/Register.component.tsx
+++ client/src/Components/Register/Register.component.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import User from '../../Interfaces/User.interface';
 import authJWT from '../../Services/UserJWT.service';
 import Cookies from 'js-cookie';
+import './Register.css';
 
 const initialState: User = {
   name: '',
@@ -18,8 +19,13 @@ interface Props {
   isAuthenticated: boolean;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Register = (_props: Props) => {
+const Register = (props: Props) => {
+  const navigate = useNavigate();
+  const routeChange = () => {
+    const path = `/admin/members`;
+    navigate(path);
+  };
+
   const [state, setState] = useState<User>(initialState);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -60,6 +66,8 @@ const Register = (_props: Props) => {
       !state.password
     );
   };
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className='w-screen h-screen sm:h-10 flex flex-col items-center bg-amber-100'>
@@ -129,13 +137,69 @@ const Register = (_props: Props) => {
           className='w-full border rounded py-2 px-3 mb-2'
         />
 
-        <button
+        {/* <button
           type='submit'
-          className=' bg-yellow-300 hover:bg-yellow-400 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300 text-black text-l font-semibold text-m px-4 py-2 border rounded-full mt-3 cursor-pointer'
+          className='bg-yellow-300 hover:bg-yellow-400 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300 text-black text-l font-semibold text-m px-4 py-2 border rounded-full mt-3 cursor-pointer'
+          disabled={validateForm()}
+        >
+          Register
+        </button> */}
+        <button
+          className='bg-yellow-300 hover:bg-yellow-400 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300 text-black text-l font-semibold text-m px-4 py-2 border rounded-full mt-3 cursor-pointer'
+          type='submit'
+          onClick={() => setShowModal(true)}
           disabled={validateForm()}
         >
           Register
         </button>
+        {showModal ? (
+          <>
+            <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+              <div className='relative w-auto my-6 mx-auto max-w-3xl'>
+                {/*content*/}
+                <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                  {/*header*/}
+                  <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
+                    {/* <h3 className='text-3xl font-semibold'>Modal Title</h3> */}
+                    {/* <button
+                      className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+                      onClick={() => setShowModal(false)}
+                    >
+                      <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
+                        ×
+                      </span>
+                    </button> */}
+                  </div>
+                  {/*body*/}
+                  <div className='relative p-6 flex-auto'>
+                    <p className='my-4 text-slate-500 text-lg leading-relaxed'>
+                      Account created successfully!
+                    </p>
+                  </div>
+                  {/*footer*/}
+                  <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+                    {/* <button
+                      className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                      type='button'
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </button> */}
+                    <button
+                      className='bg-yellow-300 hover:bg-yellow-400 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300 text-black text-l font-semibold text-m px-4 py-2 border rounded-full mt-3 cursor-pointer'
+                      type='button'
+                      // onClick={() => setShowModal(false)}
+                      onClick={routeChange}
+                    >
+                      OK
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+          </>
+        ) : null}
       </form>
     </div>
   );
